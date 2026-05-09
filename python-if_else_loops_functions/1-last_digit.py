1f13bfe88fa5200181e4b5ff304eaef335a14343
Friasru <albertofriasru@gmail.com>
2026-05-09 16:52:13 -0700

    print statement divided in the else block

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import random
+
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0 and last_digit != 0:
@@ -10,5 +11,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less "
-        "than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is "
+        "less than 6 and not 0")
