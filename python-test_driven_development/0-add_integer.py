4b1bc85352aa1cf590032b90fde68399e3fc991d
Friasru <albertofriasru@gmail.com>
2026-05-16 16:59:45 -0700

    returns the sum of a and b after type conversion and validation

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if isinstance(a, bool) or not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if isinstance(b, bool) or not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+    return a + b
