f67d0b1f59739ede2c5535f278bbf030b49c8191
Friasru <albertofriasru@gmail.com>
2026-05-16 17:14:34 -0700

    missing docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,9 @@
 This module provides a function to add two integers with type validation.
 """
 def add_integer(a, b=98):
+    """
+    Returns the sum of a and b conversion and validation.
+    """
     if isinstance(a, bool) or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if isinstance(b, bool) or not isinstance(b, (int, float)):
