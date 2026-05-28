6a08826d7ca13d5cac46485e825a655b797c88d7
Friasru <albertofriasru@gmail.com>
2026-05-28 11:18:59 -0700

    refactor integer_validator method to use f-string formatting for error messages

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,6 +9,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0.format(name)")
+            raise ValueError(f"{name} must be greater than 0")
