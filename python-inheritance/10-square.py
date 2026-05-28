4949906a8a73f42f319fcce1a3161aa70e975c15
Friasru <albertofriasru@gmail.com>
2026-05-28 14:49:10 -0700

    fix: correct method name for integer validation in Square class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,5 +6,5 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Square class that inherits from rectangle"""
     def __init__(self, size):
-        self.integer_valid("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
