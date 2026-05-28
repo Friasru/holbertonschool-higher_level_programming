0859739716782704391eb49cca32ab21134b90db
Friasru <albertofriasru@gmail.com>
2026-05-28 08:28:05 -0700

    added is_same_class function that checks class type

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_same_classs function"""
+
+
+def is_same_class(obj, a_class):
+    """Check if obj is an instance of a_class"""
+    return type (obj) == a_class
