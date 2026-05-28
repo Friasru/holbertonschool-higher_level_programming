9d1d293db9df5168ba60ce1574adcd9fa6d7f6db
Friasru <albertofriasru@gmail.com>
2026-05-28 14:54:26 -0700

    fix: correct indentation of __str__ method in Square class

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,5 +9,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
 
-    def __str__(self):
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+def __str__(self):
+        return "[Square] {}/{}".format(self._Rectangle__width,
+                                       self._Rectangle__height)
