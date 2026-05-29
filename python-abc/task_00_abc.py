35e0c61db839fd5c6572a4eb1b0dc56d48ae8f70
Friasru <albertofriasru@gmail.com>
2026-05-29 12:06:21 -0700

    feat: implement abstract base class and subclasses for Animal, Dog, and Cat

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for abstract base class example"""
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Abstract base class for animals"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return
+
+
+class Cat(Animal):
+    def sound(self):
+        return
