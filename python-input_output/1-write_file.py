4ba03c0789253f31547037ec06bb84806e18b0e4
Friasru <albertofriasru@gmail.com>
2026-06-01 09:31:19 -0700

    fix: remove unnecessary newline at the end of write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,3 @@ def write_file(filename="", text=""):
     """writes a string and returns number of characters"""
     with open(filename, mode="w", encoding='utf-8') as f:
         return f.write(text)
-
