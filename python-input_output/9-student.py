78f62cb986fc819366845a334ad29a393e04c590
Friasru <albertofriasru@gmail.com>
2026-06-06 16:29:26 -0700

    feat: add Student class with initialization and to_json method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Class that defines a student by first, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+def to_json(self):
+    return self.__dict__
