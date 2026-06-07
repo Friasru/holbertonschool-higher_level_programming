db559b03fa98460b649b0c6738e755fa2a2a1bac
Friasru <albertofriasru@gmail.com>
2026-06-06 17:06:59 -0700

    feat: implement Student class with initialization and JSON methods

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that defines a student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Class that defines a student by first, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        result = {}
+        for key in attrs:
+            if key in self.__dict__:
+                result[key] = self.__dict__[key]
+        return result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
