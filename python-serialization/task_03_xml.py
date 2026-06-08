16c7768cfce3c916e02309df3e5b18ebfcd220ff
Friasru <albertofriasru@gmail.com>
2026-06-07 18:39:11 -0700

    feat: add functions for serializing and deserializing dictionaries to and from XML

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module for serializing and deserializing python dictionaries"""
+import xml.etree.ElementTree as ET
+
+
+def sterialize_to_xml(dictionary, filename):
+    """Serialize a python dictionary"""
+    try:
+        root = ET.Element("data")
+
+        for key, value in dictionary.items():
+            child = ET.SubElement(root, key)
+            child.text = str(value)
+
+        tree = ET.ElementTree(root)
+        tree.write(filename, encoding='utf-8', xml_declaration=True)
+        return True
+
+    except (IOError, OSError):
+        return False
+    except Exception:
+        return False
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        dictionary = {}
+        for child in root:
+            dictionary[child.tag] = child.text
+
+        return dictionary
+
+    except FileNotFoundError:
+        return None
+    except ET.ParseError:
+        return None
+    except (IOError, OSError):
+        return None
+    except Exception:
+        return None
