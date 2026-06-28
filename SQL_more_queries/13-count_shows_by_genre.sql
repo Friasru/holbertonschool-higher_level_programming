57b60c73d1fc5e4f7fef274d92caad712a502e9d
Friasru <albertofriasru@gmail.com>
2026-06-28 16:08:00 -0700

    fix: change LEFT JOIN to INNER JOIN in SQL script for counting shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows
 SELECT tv_shows.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id, tv_genres.name
 ORDER BY number_of_shows DESC;
