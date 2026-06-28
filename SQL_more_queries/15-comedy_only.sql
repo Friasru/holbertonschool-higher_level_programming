efe1a4a6a0e319cce5fb84b36a7ae2ab4136f092
Friasru <albertofriasru@gmail.com>
2026-06-28 16:14:51 -0700

    fix: correct table reference in SQL script for listing Comedy shows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Script that lists all Comedy shows from hbtn_0d_tvshows
-SELECT tv_genres.title
+SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
