7f8615f0e489a5ccb08873ed4a6a1769d2c92134
Friasru <albertofriasru@gmail.com>
2026-06-28 17:04:04 -0700

    fix: update shebang and improve script documentation for listing states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Script that lists all states from the hbtn_0e_0_usa database
+Script to list all states from the hbtn_0e_0_usa database
 """
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-
+    
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,15 +17,15 @@ if __name__ == "__main__":
         passwd=mysql_password,
         db=database_name
     )
-
+    
     cursor = db.cursor()
-
+    
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
+    
     states = cursor.fetchall()
-
+    
     for state in states:
         print(state)
-
+    
     cursor.close()
     db.close()
